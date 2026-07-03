46a63d9a1f2deef5c3051a4abd767151770ab73a
YTFirestormHD <moritzvongruber@gmail.com>
2026-07-03 02:33:24 +0200

    testing
    Tested a bit with input buffering as well as the world switching

--- worlds/test_moritz/player.gd
+++ worlds/test_moritz/player.gd
@@ -1,17 +1,27 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 250.0
 const JUMP_VELOCITY = -400.0
 
 
+var passed_frames = 0
+var buffered
+
+
 func _physics_process(delta: float) -> void:
+	if buffered:
+		passed_frames += 1
+	if passed_frames == 4:
+		buffered = false
+		passed_frames = 0
+	 
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if (buffered or Input.is_action_pressed("ui_accept")) and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
@@ -23,3 +33,8 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action("ui_accept"):
+		buffered = true
--- worlds/test_moritz/test_of_tmls.gd
+++ worlds/test_moritz/test_of_tmls.gd
@@ -2,6 +2,8 @@ extends Node2D
 @onready var tml_1: TileMapLayer = $TileMapLayer
 @onready var tml_2: TileMapLayer = $TileMapLayer2
 
+var locked = false
+var locked_timer = 0
 
 
 # Called when the node enters the scene tree for the first time.
@@ -11,7 +13,8 @@ func _ready() -> void:
 
 
 func _input(event: InputEvent) -> void:
-	if Input.is_action_pressed("interact_e"):
+	if Input.is_action_pressed("interact_e") and not locked:
+		locked = true
 		if tml_1.visible == true:
 			tml_1.visible = false
 			tml_2.visible = true
@@ -27,4 +30,8 @@ func _input(event: InputEvent) -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if locked:
+		locked_timer += 1
+	if locked_timer == 60:
+		locked = false
+		locked_timer = 0
