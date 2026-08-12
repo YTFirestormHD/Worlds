63cb11a3fa4c115c10d073ea8eb04e52b01592ef
Finja <finja.wede@gmail.com>
2026-08-12 10:49:44 +0200

    fixed interactive tile still interacting with player when in other map

--- worlds/test_moritz/test_of_tmls.gd
+++ worlds/test_moritz/test_of_tmls.gd
@@ -8,23 +8,17 @@ var locked_timer = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	tml_1.visible = true
-	tml_2.visible = false
+	tml_1.enabled = true
+	tml_2.enabled = false
 
 
 func _input(event: InputEvent) -> void:
 	if Input.is_action_pressed("interact_e") and not locked:
 		locked = true
-		if tml_1.visible == true:
-			tml_1.visible = false
-			tml_2.visible = true
-			tml_1.collision_enabled = tml_1.visible
-			tml_2.collision_enabled = tml_2.visible
-		else:
-			tml_1.visible = true
-			tml_2.visible = false
-			tml_1.collision_enabled = tml_1.visible
-			tml_2.collision_enabled = tml_2.visible
+		
+		tml_2.enabled = tml_1.enabled
+		tml_1.enabled = not tml_1.enabled
+		
 		print("success")
 
 
